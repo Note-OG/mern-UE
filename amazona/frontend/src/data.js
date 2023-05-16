a7af798b6af20d13bd6c8428032376e09bafe925
Ramon_ Bulan <gamegrind325@gmail.com>
2023-05-16 15:30:32 +0800

    #

---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike slim shirt',
+      category: 'Shirts',
+      images: '/images/1.png', //679px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike shirt',
+      slug: 'nike shirt',
+      category: 'Shirts',
+      images: '/images/2.png', //679px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: ' shirt',
+      slug: ' shirt',
+      category: 'Shirts',
+      images: '/images/3.png', //679px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike',
+      slug: 'nike',
+      category: 'Shirts',
+      images: '/images/4.png', //679px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+  ],
+};
+export default data;
